9d95f51306d3514fce5f942c02934a5a008607f9
Devops-Ramakrishna <gamparamakrishna999@gmail.com>
2023-12-13 14:37:55 +0530

    i am practicing git

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PERSON1=ramesh
+PERSON2=suresh
+
+echo " $PERSON1 : hi $PERSON2 , what about you"
+echo " $PERSON2 : hi $PERSON1 , i am good"
+echo " $PERSON1 : where are you from?"
+echo " $PERSON2 : i am from kadapa" 
---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+PERSON1=$1
+PERSON2=$2
+
+echo " $PERSON1 : hi $PERSON2 , what about you"
+echo " $PERSON2 : hi $PERSON1 , i am good"
+echo " $PERSON1 : where are you from?"
+echo " $PERSON2 : i am from kadapa" 
