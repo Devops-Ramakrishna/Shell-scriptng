d5140465a4efeab8e0064fc2a89d542b23e6b88e
Devops-Ramakrishna <gamparamakrishna999@gmail.com>
2023-12-13 15:02:41 +0530

    i am practicing git

---
+++ 09-Arrays.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+FRUITS=("APPLE" "BANANA" "SWEET")
+
+echo " FIRST friuit is : ${FRUITS[0]} "
+
+echo " SECOND friuit is : ${FRUITS[1]} "
+
+echo " THIRD friuit is : ${FRUITS[2]} "
+
+echo " ALL friuit is : ${FRUITS[@]} "
