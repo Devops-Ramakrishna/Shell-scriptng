a6b4c3b5555efea7d0bc623aa4d5ab3c14cc0b25
Devops-Ramakrishna <gamparamakrishna999@gmail.com>
2023-12-17 14:56:06 +0530

    i am practicing git

---
+++ 15-install-packages.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+if [ $ID -ne 0 ]
+then
+    echo " $R ERROR : run this script with root user $N "
+    exit 1 # you can give other than 0
+else
+    echo " you are root user "
+fi # fi means reverse of fi, it indicating condition end
+
+echo " All arguments : $@ "
+
+
