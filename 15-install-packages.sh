3e1e7769b90b3e0395c80d2c1e0581d34a236ed7
Devops-Ramakrishna <gamparamakrishna999@gmail.com>
2023-12-17 15:14:06 +0530

    i am practicing git

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -6,6 +6,14 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e " $2 .... $R FAILED $N "
+    else
+        echo -e " $2 .... $G SUCCESS $N "
+    fi
+
 
 if [ $ID -ne 0 ]
 then
@@ -15,6 +23,22 @@ else
     echo " you are root user "
 fi # fi means reverse of fi, it indicating condition end
 
-echo " All arguments : $@ "
+#echo " All arguments : $@ "
+
+# mysql git postfix
+
+# package=git for girst time
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0]
+    then
+        yum install $package -y
+        VALIADTE $? "INSTALLATION OF $package"
+done
+
+
+
 
 
